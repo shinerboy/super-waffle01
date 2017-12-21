5aed7550a1a7f232e6016758aad620a400126231
shinerboy <34726251+shinerboy@users.noreply.github.com>
2017-12-20 19:19:54 -0600

    Add files via upload

---
+++ WhileLoop_Bucky.cpp
@@ -0,0 +1,21 @@
+// make a program where a user can enter a number 5 times
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int x = 1;
+int number;
+int total = 0;
+
+while(x <= 5){
+	/*cout << x << endl;*/
+	cin >> number;
+	total = total + number; 
+x++;
+}
+
+cout <<"Your total is: " << total << endl;
+return 0;
+}
